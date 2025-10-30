68e4804a7eaa367d7db3699d1606563d4ae31c11
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-10-30 15:17:55 +0100

    header responsive

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = function () {
   return (
-    <div className="text-4xl text-white font-bold pl-15 pt-8">
+    <div className="text-white font-bold pl-4 pt-4 sm:pl-6 sm:pt-6 md:pl-8 md:pt-8 lg:pl-12 lg:pt-10 text-2xl sm:text-3xl md:text-4xl lg:text-5xl">
       <h1>Climys</h1>
     </div>
   )
