ad77cae896716f04b6349adb1d34a2a70474d63b
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-10-30 12:43:28 +0100

    start

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css"
+
+function App() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-blue-500 text-white text-3xl">
+      Climys è pronto! 🌤️
+    </div>
+  )
+}
+
+export default App
