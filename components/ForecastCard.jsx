94ecbdc4323dddc57cd8c9c3bc8f8d506c5f1daa
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-11-03 13:12:42 +0100

    Update ForecastCard.jsx

--- components/ForecastCard.jsx
+++ components/ForecastCard.jsx
@@ -4,13 +4,13 @@ const ForecastCard = ({ data }) => {
   const days = ["Dom", "Lun", "Mar", "Mer", "Gio", "Ven", "Sab"]
 
   return (
-    <div className="w-full max-w-5xl mx-auto mt-10 px-4">
+    <div className="w-full max-w-full mx-auto mt-10 px-4">
       <h2 className="text-2xl text-white font-semibold mb-4 text-center">
         Previsioni giornaliere
       </h2>
       <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-7 gap-4">
         {data.map((day, index) => {
-          const date = new Date(day.dt)
+          const date = new Date(day.dt * 1000)
           const dayName = days[date.getDay()]
           return (
             <div
