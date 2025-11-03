345814ea3988d17f75779a1d9ad1734d08d3550a
SalvatoreGianquinto <gianquinto.salvatore11@gmail.com>
2025-11-03 12:58:22 +0100

    fix

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,6 @@
 const Header = function () {
   return (
-    <div className="text-white font-bold pl-4 pt-4 sm:pl-6 sm:pt-6 md:pl-8 md:pt-8 lg:pl-12 lg:pt-5 text-2xl sm:text-3xl md:text-4xl lg:text-4xl">
+    <div className="text-white font-bold pl-4 pt-4 sm:pl-6 sm:pt-6 md:pl-8 md:pt-8 lg:pl-12 lg:pt-5 text-2xl sm:text-3xl md:text-4xl lg:text-4xl sm:font-sans">
       <h1>Climys</h1>
     </div>
   )
